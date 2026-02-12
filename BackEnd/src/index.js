c41c7ef3553b1562fd54593fee3a12a3575f081d
Harish-987 <harishmacha987@gmail.com>
2026-02-12 18:04:56 +0000

    cors update for production

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -19,10 +19,17 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cookieParser());
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials:true
+// }));
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.NODE_ENV === "production"
+        ? "https://foodc.onrender.com"
+        : "http://localhost:5173",
+    credentials: true
 }));
+
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
